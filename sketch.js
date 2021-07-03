c345b6e7e19928ebb221fec2b2dfbc4bc2bee7a0
zb0903 <66878170+zb0903@users.noreply.github.com>
2021-07-03 17:20:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ var tom, jerry;
 
 function preload() {
     //load the images here
-    normalCatImage = loadImage("images/cat1.png");
-    catImage = loadImage("images/cat2.png","images/cat3.png")
-    happyCatImage = loadImage("images/cat4.png");
-    normalMouseImage = loadImage("images/mouse1.png")
-    mouseImage = loadImage("images/mouse2.png","images/mouse3.png");
-    happyMouseImage = loadImage("images/mouse4.png");
+    normalCatImage = loadAnimation("images/cat1.png");
+    catImage = loadAnimation("images/cat2.png","images/cat3.png")
+    happyCatImage = loadAnimation("images/cat4.png")
+    normalMouseImage = loadAnimation("images/mouse1.png");
+    mouseImage = loadAnimation("images/mouse2.png","images/mouse3.png");
+    happyMouseImage = loadAnimation("images/mouse4.png");
     gardenImage = loadImage("images/garden.png");
 }
 
@@ -16,15 +16,15 @@ function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
     tom = createSprite(900,700);
-    tom.addImage("resting",normalCatImage);
-    tom.addImage("walking",catImage);
-    tom.addImage("happy",happyCatImage);
+    tom.addAnimation("resting",normalCatImage);
+    tom.addAnimation("walking",catImage);
+    tom.addAnimation("happy",happyCatImage);
     tom.scale = 0.15
 
     jerry = createSprite(400,700);
-    jerry.addImage("normal",normalMouseImage);
-    jerry.addImage("teasing",mouseImage);
-    jerry.addImage("happy",happyMouseImage);
+    jerry.addAnimation("normal",normalMouseImage);
+    jerry.addAnimation("teasing",mouseImage);
+    jerry.addAnimation("happy",happyMouseImage);
     jerry.scale = 0.1
 }
 
@@ -32,21 +32,24 @@ function draw() {
 
     background(gardenImage);
 
-    if(keyDown("space")){
-        jerry.changeImage("teasing",mouseImage);
-     }
  
-    if(keyDown("left")){
-         tom.changeImage("walking",catImage);
-         tom.velocityX = -2;
-         tom.scale = 0.2;
-    }
     //Write condition here to evalute if tom and jerry collide
     if(tom.x-jerry.x < (tom.width - jerry.width)/2){
-        tom.changeAnimaton("happy",happyCatImage);
+        tom.changeAnimation("happy",happyCatImage);
+        jerry.changeAnimation("happy",happyMouseImage)
         tom.velocityX = 0;
     }
 
     drawSprites();
         
+}
+function keyPressed(){
+    if (keyCode === LEFT_ARROW){
+        tom.velocityX = -4;
+        tom.changeAnimation("walking",catImage);
+        tom.scale = 0.2;
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("teasing",mouseImage);
+    }
+
 }
